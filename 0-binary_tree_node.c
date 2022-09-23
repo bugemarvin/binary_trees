6fb6930695e8f87d95812e1bb162293cf8a0cd04
Marvin Kurland <bugemarvin@outlook.com>
2022-09-23 18:30:22 +0300

    initial for task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_node - c function to create a tree node.
+  * @parent: pointer to the root node created.
+  * @value: data to be initialized on the new node
+  *
+  * Return: Always 0 (pointer for the created new node)
+  *			-1 or NULL for failure
+  */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	return (parent);
+}
