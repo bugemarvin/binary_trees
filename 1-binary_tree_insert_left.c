49d2cc5c3778db8d045ea77cc8c8da1e02834307
Marvin Kurland <bugemarvin@outlook.com>
2022-09-24 02:28:42 +0300

    inserting the left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,8 +13,10 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *l_child = binary_tree_node(parent, value);
+
 	if (l_child == NULL || parent == NULL)
 		return (NULL);
+
 	if (l_child != NULL)
 	{
 		if (parent->left != NULL)
