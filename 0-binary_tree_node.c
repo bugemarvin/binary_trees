3489e1e221a882b7e8ef93c3918690ff2ab7aa92
Marvin Kurland <bugemarvin@outlook.com>
2022-09-24 00:52:03 +0300

    creating a new node for btree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,15 +11,18 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	parent = malloc(sizeof(binary_tree_t));
-	
-	if (!parent)
-		return (NULL);
-	if (parent != NULL)
+	binary_tree_t *root = malloc(sizeof(binary_tree_t));
+
+	if (root == NULL)
 	{
-		(*parent).left = NULL;
-		(*parent).right = NULL;
-		(*parent).n = value;
+		return (NULL);
 	}
-	return (parent);
+	if (root != NULL)
+	{
+		(*root).left = NULL;
+		(*root).right = NULL;
+		(*root).n = value;
+		(*root).parent = parent;
+	}
+	return (root);
 }
